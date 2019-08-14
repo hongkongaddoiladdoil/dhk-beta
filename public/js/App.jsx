d3b07e173d16b3ac3ed9d57d579043047bf9dbf9
francis <hongkongaddoiladdoil@gmail.com>
2019-08-14 14:32:59 -0700

    add post form

--- public/js/App.jsx
+++ public/js/App.jsx
@@ -1,43 +1,71 @@
 import React from 'react'
-import ReactBootstrap, {Jumbotron, Button, Col, Grid, Panel, FormGroup, ButtonToolbar} from 'react-bootstrap'
+import ReactBootstrap, {Jumbotron, Button, Col, Grid, Panel, Form, FormGroup, ButtonToolbar} from 'react-bootstrap'
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
-const API_URL = window.location.href;
-
 class App extends React.Component {
-  state = {
-    question: []
-  };
   constructor(props) {
     super(props);
+    this.state = {
+      question: [],
+      questionToSubmit: '',
+      userIdToSubmit: ''
+    };
   }
   componentDidMount() {
-    const url = `${API_URL}question/`;
-    axios.get(url).then(response => response.data.data)
-    .then((data) => {
-      this.setState({ question: data })
-     })
+    axios.get('/question').then(response => response.data.data)
+      .then((data) => {
+        this.setState({ question: data })
+      })
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit = (e) => {
+    e.preventDefault();
+    // get our form data out of state
+    const userIdToSubmit = this.state.userIdToSubmit;
+    const questionToSubmit = this.state.questionToSubmit;
+
+    axios.post('/question', { user_id: userIdToSubmit, content: questionToSubmit })
+      .then((result) => {
+        console.log(result);
+      });
   }
   render() {
+    const userIdToSubmit = this.state.userIdToSubmit;
+    const questionToSubmit = this.state.questionToSubmit;
     return (
-       <div className="container">
+      <div className="container">
         <div className="col-xs-8">
-        <h1>List of Question</h1>
-        {this.state.question.map((question) => (
-          <div className="card">
-           <div className="card-body">
-               <h5 className="card-title">User: {question.user_id} Q: {question.content}</h5>
+          <h1>List of Question</h1>
+          {this.state.question.map((question) => (
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">User: {question.user_id} Q: {question.content}</h5>
+              </div>
             </div>
+          ))}
+          <br/>
+          <div>
+            <Form onSubmit={this.onSubmit}>
+              <Form.Group>
+                <Form.Control type="text" placeholder="User ID" name="userIdToSubmit" value={userIdToSubmit} onChange={this.onChange}/>
+                <Form.Label>Question</Form.Label>
+                <Form.Control as="textarea" rows="3"
+                  name="questionToSubmit"
+                  value={questionToSubmit}
+                  placeholder = "Question"
+                  onChange={this.onChange} />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
           </div>
-        ))}
-        <div>
-        <ButtonToolbar>
-        <Button variant="primary">Primary</Button>
-        </ButtonToolbar>
         </div>
-        </div>
-       </div>
+      </div>
     )
   }
 };
