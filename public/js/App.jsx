5e2066ee4438c768f2492b8c505688cad5987a08
francis <hongkongaddoiladdoil@gmail.com>
2019-08-14 13:05:24 -0700

    fix warning

--- public/js/App.jsx
+++ public/js/App.jsx
@@ -1,20 +1,44 @@
 import React from 'react'
-import { Button, ButtonToolbar } from 'react-bootstrap'
+import ReactBootstrap, {Jumbotron, Button, Col, Grid, Panel, FormGroup, ButtonToolbar} from 'react-bootstrap'
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
-// function App() (
-//   <div>
-//   <Button bsStyle='primary' bsSize='large'>TEST</Button>
-//   </div>
-// )
+const API_URL = window.location.href;
+
 class App extends React.Component {
+  state = {
+    question: []
+  };
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    const url = `${API_URL}question/`;
+    axios.get(url).then(response => response.data.data)
+    .then((data) => {
+      this.setState({ question: data })
+     })
+  }
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
+       <div className="container">
+        <div className="col-xs-8">
+        <h1>List of Question</h1>
+        {this.state.question.map((question) => (
+          <div className="card">
+           <div className="card-body">
+               <h5 className="card-title">User: {question.user_id} Q: {question.content}</h5>
+            </div>
+          </div>
+        ))}
+        <div>
+        <ButtonToolbar>
+        <Button variant="primary">Primary</Button>
+        </ButtonToolbar>
+        </div>
+        </div>
+       </div>
+    )
   }
 };
 
