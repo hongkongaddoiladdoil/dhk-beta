33665a05c6eeb26fe90ef9f53cb6157216a3e79d
francis <hongkongaddoiladdoil@gmail.com>
2019-08-15 13:01:06 -0700

    able to send email

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,9 @@
 const question = require('./question/question.service.js');
+const users = require('./users/users.service.js');
+const mailer = require('./mailer/mailer.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(question);
+  app.configure(users);
+  app.configure(mailer);
 };
